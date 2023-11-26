46425c3298cb76b11f31344d038399e7f18314ad
Ahmad Faraaz Jafri <ahmadfaraazjafri@gmail.com>
2023-11-26 13:51:25 -0500

    udp

--- src/main/java/org/example/UDPServer.java
+++ src/main/java/org/example/UDPServer.java
@@ -1,8 +1,7 @@
 package org.example;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
@@ -14,11 +13,12 @@ import static java.nio.channels.SelectionKey.OP_READ;
 public class UDPServer {
 
     private static long lastReceivedSequenceNumber = -1;
-    private static boolean handshakeComplete = false;
+    private static long serverSequenceNumber = 2000; // Initial server sequence number
+    private static long expectedDataSequenceNumber = -1;
 
     public static void main(String[] args) throws IOException {
         InetSocketAddress serverAddress = new InetSocketAddress("localhost", 8007);
-
+        InetSocketAddress routerAddress = new InetSocketAddress("localhost", 3000);
         try (DatagramChannel channel = DatagramChannel.open()) {
             channel.bind(serverAddress);
             channel.configureBlocking(false);
@@ -39,87 +39,100 @@ public class UDPServer {
                         DatagramChannel readChannel = (DatagramChannel) key.channel();
                         ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
                         SocketAddress clientAddress = readChannel.receive(buf);
+                        System.out.println(clientAddress);
                         buf.flip();
                         Packet receivedPacket = Packet.fromBuffer(buf);
 
-                        handleReceivedPacket(readChannel, clientAddress, receivedPacket);
+                        handleReceivedPacket(readChannel, clientAddress, receivedPacket, routerAddress);
                     }
 
                     keyIterator.remove();
                 }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static void handleReceivedPacket(DatagramChannel channel, SocketAddress clientAddress, Packet receivedPacket) throws IOException {
-        // Handle the received packet based on its type
+    private static void handleReceivedPacket(DatagramChannel channel, SocketAddress clientAddress, Packet receivedPacket, InetSocketAddress routerAddress) throws Exception {
         switch (receivedPacket.getType()) {
             case Packet.SYN:
-                System.out.println("Server: SYN packet received from client");
-                handleSynPacket(channel, clientAddress);
+                System.out.println("Server: SYN packet received from client. Sequence Number: " + receivedPacket.getSequenceNumber());
+                handleSynPacket(channel, (InetSocketAddress) clientAddress, routerAddress);
                 break;
             case Packet.ACK:
                 handleAckPacket(channel, clientAddress, receivedPacket);
                 break;
             case Packet.DATA:
-                if (handshakeComplete) {
+                if (handshakeComplete()) {
                     handleDataPacket(channel, clientAddress, receivedPacket);
                 } else {
                     System.out.println("Server: Ignoring DATA packet. Handshake not completed.");
                 }
                 break;
-            // Add more cases as needed for other packet types
             default:
                 System.out.println("Server: Unexpected packet type received.");
         }
     }
 
-    private static void handleSynPacket(DatagramChannel channel, SocketAddress clientAddress) throws IOException {
-        // Respond with SYN-ACK
-        Packet synAckPacket = new Packet.Builder()
-                .setType(Packet.SYN_ACK)
-                .setSequenceNumber(1L)  // You may need to adjust the sequence number
-                .setPortNumber(((InetSocketAddress) channel.getLocalAddress()).getPort())
-                .setPeerAddress(((InetSocketAddress) clientAddress).getAddress())
-                .setPayload("SYN-ACK".getBytes())
-                .create();
-
-        channel.send(synAckPacket.toBuffer(), clientAddress);
-        System.out.println("Server: SYN-ACK packet sent to client.");
+    private static void handleSynPacket(DatagramChannel channel, InetSocketAddress clientAddress, InetSocketAddress routerAddress) throws Exception {
+        DatagramPacket synAckPacket = constructPacket((byte) Packet.SYN_ACK, serverSequenceNumber, clientAddress.getAddress(),clientAddress.getPort(), "SYN-ACK".getBytes());
+        sendPacket(synAckPacket, routerAddress);
+        System.out.println("Server: SYN-ACK packet sent to client. Sequence Number: " + serverSequenceNumber);
     }
 
+    private static void sendPacket(DatagramPacket packet, InetSocketAddress destination) throws Exception {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.send(packet);
+            System.out.println("Sent packet to: " + destination);
+        }
+    }
     private static void handleAckPacket(DatagramChannel channel, SocketAddress clientAddress, Packet ackPacket) {
-        // Handle ACK packet as needed
-        System.out.println("Server: Received ACK packet from client. Handshake complete.");
+        System.out.println("Server: Received ACK packet from client. Handshake complete. Sequence Number: " + ackPacket.getSequenceNumber());
         lastReceivedSequenceNumber = ackPacket.getSequenceNumber();
-        handshakeComplete = true;
-        // Continue with the rest of your application logic
+        System.out.println("Server: Last Received Sequence Number: " + lastReceivedSequenceNumber);
     }
 
     private static void handleDataPacket(DatagramChannel channel, SocketAddress clientAddress, Packet dataPacket) throws IOException {
-        // Handle DATA packet as needed
-        System.out.println("Server: Received DATA packet from client. Sequence Number: " + dataPacket.getSequenceNumber());
-
-        // Process the received data (you can replace this with your custom logic)
-        String receivedData = new String(dataPacket.getPayload());
-        System.out.println("Server: Received data: " + receivedData);
-
-        // Respond with a DATA_ACK
-        sendDataAckPacket(channel, clientAddress, dataPacket.getSequenceNumber());
+        if (dataPacket.getSequenceNumber() == expectedDataSequenceNumber) {
+            System.out.println("Server: Received DATA packet from client. Sequence Number: " + dataPacket.getSequenceNumber());
+            String receivedData = new String(dataPacket.getPayload());
+            System.out.println("Server: Received data: " + receivedData);
+            sendDataAckPacket(channel, clientAddress, dataPacket.getSequenceNumber());
+            expectedDataSequenceNumber++;
+        } else {
+            System.err.println("Server: Out-of-order packet received. Ignoring. Expected: " + expectedDataSequenceNumber + ", Received: " + dataPacket.getSequenceNumber());
+        }
     }
 
     private static void sendDataAckPacket(DatagramChannel channel, SocketAddress clientAddress, long sequenceNumber) throws IOException {
-        // Respond with DATA_ACK
         Packet dataAckPacket = new Packet.Builder()
                 .setType(Packet.DATA_ACK)
                 .setSequenceNumber(sequenceNumber)
                 .setPortNumber(((InetSocketAddress) channel.getLocalAddress()).getPort())
                 .setPeerAddress(((InetSocketAddress) clientAddress).getAddress())
-                .setPayload("DATA_ACK".getBytes())  // You can customize the payload if needed
+                .setPayload("DATA_ACK".getBytes())
                 .create();
 
-        // Send the DATA_ACK packet
         channel.send(dataAckPacket.toBuffer(), clientAddress);
         System.out.println("Server: Sent DATA_ACK packet to client. Sequence Number: " + sequenceNumber);
+    }
+
+    private static boolean handshakeComplete() {
+        return lastReceivedSequenceNumber >= 0;
+    }
+
+    private static DatagramPacket constructPacket(byte packetType, long sequenceNumber,
+                                                  InetAddress peerAddress, int peerPort, byte[] payload) {
+        int totalSize = 1 + 8 + 4 + 2 + payload.length;
+
+        ByteBuffer buffer = ByteBuffer.allocate(totalSize);
+        buffer.put(packetType);
+        buffer.putLong(sequenceNumber);
+        buffer.put(peerAddress.getAddress());
+        buffer.putShort((short) peerPort);
+        buffer.put(payload);
+
+        return new DatagramPacket(buffer.array(), buffer.position(), peerAddress, peerPort);
     }
 }
